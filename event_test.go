0f0ef6e71fe2615ebf2afa58308cbee88ba22943
Christopher Kleine <chris@suletuxe.de>
2023-09-12 14:40:21 +0200

    test: Added name to Event.String() tests

--- event_test.go
+++ event_test.go
@@ -9,22 +9,25 @@ import (
 
 func TestEventString(t *testing.T) {
 	testTable := []struct {
+		name     string
 		input    *sse.Event
 		expected string
 	}{
-		{input: &sse.Event{Data: "test"}, expected: "data: test\n\n"},
-		{input: &sse.Event{Retry: 1 * time.Second}, expected: "retry: 1000\n\n"},
-		{input: &sse.Event{ID: "1"}, expected: "id: 1\n\n"},
-		{input: &sse.Event{Event: "foo"}, expected: "event: foo\n\n"},
-		{input: &sse.Event{Retry: 1 * time.Second, Data: "Dummy"}, expected: "data: Dummy\nretry: 1000\n\n"},
-		{input: &sse.Event{Data: "foo\nbar"}, expected: "data: foo\ndata: bar\n\n"},
-		{input: &sse.Event{Data: "foo\nbar\n"}, expected: "data: foo\ndata: bar\n\n"},
+		{name: "data-only", input: &sse.Event{Data: "test"}, expected: "data: test\n\n"},
+		{name: "retry-only", input: &sse.Event{Retry: 1 * time.Second}, expected: "retry: 1000\n\n"},
+		{name: "id-only", input: &sse.Event{ID: "1"}, expected: "id: 1\n\n"},
+		{name: "event-only", input: &sse.Event{Event: "foo"}, expected: "event: foo\n\n"},
+		{name: "rety-and-data", input: &sse.Event{Retry: 1 * time.Second, Data: "Dummy"}, expected: "data: Dummy\nretry: 1000\n\n"},
+		{name: "multiline-data", input: &sse.Event{Data: "foo\nbar"}, expected: "data: foo\ndata: bar\n\n"},
+		{name: "multiline-data-with-eol", input: &sse.Event{Data: "foo\nbar\n"}, expected: "data: foo\ndata: bar\n\n"},
 	}
 
 	for _, testCase := range testTable {
-		actual := testCase.input.String()
-		if actual != testCase.expected {
-			t.Errorf("%q should be %q", actual, testCase.expected)
-		}
+		t.Run(testCase.name, func(t *testing.T) {
+			actual := testCase.input.String()
+			if actual != testCase.expected {
+				t.Errorf("%q should be %q", actual, testCase.expected)
+			}
+		})
 	}
 }
